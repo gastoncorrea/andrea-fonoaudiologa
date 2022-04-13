8a90cb36f5bf39a0188fdf3672230fa37132f168
Gaston <gastoncorrea90e@gmail.com>
2022-04-13 11:32:12 -0300

    Se centro contenido de navbar y se creo componente para hacer el mensaje de bienvenida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
